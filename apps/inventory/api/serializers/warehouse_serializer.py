a80eb297f0c8e5a7740b78eccfc9faeba592881a
mangelryujin <mangelryujin@gmail.com>
2025-11-01 11:55:20 -0400

    fix: change name StoreSerializer to WarehouseStoreSerializer in warehouse serializer

--- apps/inventory/api/serializers/warehouse_serializer.py
+++ apps/inventory/api/serializers/warehouse_serializer.py
@@ -2,17 +2,16 @@ from rest_framework import serializers
 from apps.inventory.models import Store, Warehouse
 
 
-class StoreSerializer(serializers.ModelSerializer):
+class WarehouseStoreSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Store
         fields = ("id", "name")
         
 class WarehouseSerializer(serializers.ModelSerializer):
-    store_detail = StoreSerializer(source="store", read_only=True)
+    store_detail = WarehouseStoreSerializer(source="store", read_only=True)
     
     class Meta:
         model = Warehouse
         fields = '__all__'
         read_only_fields = ('created_at', 'updated_at')
-
